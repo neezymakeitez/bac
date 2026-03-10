87c07769e30e20394c5a86bd066a43139ffaa7fa
Nihal Boina <nihal.boina@gmail.com>
2026-03-10 16:17:26 -0700

    done

--- bac/.next/static/chunks/Development_LEVSYS_bac_bac_src_components_5cde6ee6._.js
+++ bac/.next/static/chunks/Development_LEVSYS_bac_bac_src_components_5cde6ee6._.js
@@ -366,7 +366,7 @@ function HeroHeading(param) {
                         },
                         "aria-hidden": true,
                         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$Development$2f$LEVSYS$2f$bac$2f$bac$2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$Development$2f$LEVSYS$2f$bac$2f$bac$2f$node_modules$2f$next$2f$image$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
-                            src: "/bac_logo.svg",
+                            src: "/cal.svg",
                             alt: "",
                             fill: true,
                             sizes: "1em",
--- bac/src/components/HeroHeading.tsx
+++ bac/src/components/HeroHeading.tsx
@@ -56,7 +56,7 @@ export default function HeroHeading({
           aria-hidden
         >
           <Image
-            src="/bac_logo.svg"
+            src="/cal.svg"
             alt=""
             fill
             sizes="1em"
